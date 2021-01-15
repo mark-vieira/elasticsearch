88fa315697b845981c3915ca49fd0199a28e2aa5
Mark Vieira <portugee@gmail.com>
2021-01-15 14:56:23 -0800

    Replace Apache 2.0 license header with SSPL+Elastic license header

--- buildSrc/src/integTest/groovy/org/elasticsearch/gradle/internal/precommit/LicenseHeadersPrecommitPluginFuncTest.groovy
+++ buildSrc/src/integTest/groovy/org/elasticsearch/gradle/internal/precommit/LicenseHeadersPrecommitPluginFuncTest.groovy
@@ -1,20 +1,9 @@
 /*
- * Licensed to Elasticsearch under one or more contributor
- * license agreements. See the NOTICE file distributed with
- * this work for additional information regarding copyright
- * ownership. Elasticsearch licenses this file to you under
- * the Apache License, Version 2.0 (the "License"); you may
- * not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * and the Server Side Public License, v 1; you may not use this file except in
+ * compliance with, at your election, the Elastic License or the Server Side
+ * Public License, v 1.
  */
 
 package org.elasticsearch.gradle.internal.precommit
@@ -54,7 +43,7 @@ class LicenseHeadersPrecommitPluginFuncTest extends AbstractGradleFuncTest {
             id 'java'
             id 'elasticsearch.internal-licenseheaders'
         }
-        
+
         tasks.named("licenseHeaders").configure {
             excludes << 'org/acme/filtered/**/*'
         }
@@ -90,7 +79,7 @@ class LicenseHeadersPrecommitPluginFuncTest extends AbstractGradleFuncTest {
  */
 
  package ${packageString(sourceFile)};
- 
+
  public class ${sourceFile.getName() - ".java"} {
  }
  """
@@ -102,9 +91,9 @@ class LicenseHeadersPrecommitPluginFuncTest extends AbstractGradleFuncTest {
 /*
  * Blubb my custom license shrug!
  */
- 
+
  package ${packageString(sourceFile)};
- 
+
  public class ${sourceFile.getName() - ".java"} {
  }
  """
@@ -113,25 +102,14 @@ class LicenseHeadersPrecommitPluginFuncTest extends AbstractGradleFuncTest {
     private File apacheSourceFile() {
         file("src/main/java/org/acme/ApacheLicensed.java") << """
 /*
- * Licensed to Elasticsearch under one or more contributor
- * license agreements. See the NOTICE file distributed with
- * this work for additional information regarding copyright
- * ownership. Elasticsearch licenses this file to you under
- * the Apache License, Version 2.0 (the "License"); you may
- * not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * and the Server Side Public License, v 1; you may not use this file except in
+ * compliance with, at your election, the Elastic License or the Server Side
+ * Public License, v 1.
  */
- 
- package org.acme; 
+
+ package org.acme;
  public class ApacheLicensed {
  }
  """
